59c477b5a96138d85716a5b3ebeb7298cc80bd58
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-17 15:51:14 +0530

    starting of the apple website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+
+
+function App() {
+  
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
