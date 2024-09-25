0a2f3558334bb1ea64823f5c0303ee43dad63d8d
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-25 16:20:02 +0530

    added features section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
+import Features from './components/Features';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   )
 }
--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,5 +1,18 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
+export const animateWithGsap = (target, animationProps , scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: 'restart reverse restart reverse',
+      start: 'top 80%',
+    ...scrollProps
+  }
+})
+}
 
 export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
     timeline.to(rotationRef.current.rotation, {
