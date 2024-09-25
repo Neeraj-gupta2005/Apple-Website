d4cba3ac872f4f84b45d24c58d7d4b9ab3c7c88f
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-25 15:49:45 +0530

    completed 3d section

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -10,7 +10,7 @@ const ModelView = ({ index , groupRef , gsapType ,controlRef , setRotationState 
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
     >
       {/* Ambient light */}
       <ambientLight intensity={0.3} />
