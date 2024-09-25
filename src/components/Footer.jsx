c402c6baf6898e971c4a38721e90f8de05b162e3
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-25 17:24:58 +0530

    footer completed hence the website is done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,16 +24,17 @@ const Footer = () => {
 
         <div className='bg-neutral-700 my-5 h-[1px] w-full'/>
 
-        <div className='flex md:flex-row flex-col md:items-center justify-between'>
-          <p className='font-semibold text-gray text-xs'>
-            Copyright @ 2024 Apple Inc. All rights reserved.
-          </p>
-          <div className='flex'>
-            {footerLinks.map((link,i)=>{
-              <p className='font-semibold text-gray text-xs' key={link}>{link}{' '}
-              {i !== footerLinks.length - 1 && (<span className='mx-2'> | </span>)}
+        <div className="flex md:flex-row flex-col md:items-center justify-between">
+          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
+          <div className="flex">
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}{' '}
+                {i !== footerLinks.length - 1 && (
+                  <span className="mx-2"> | </span>
+                )}
               </p>
-            })}
+            ))}
           </div>
         </div>
       </div>
