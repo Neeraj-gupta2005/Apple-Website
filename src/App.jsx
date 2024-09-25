243f123387dbea5698164a117b61d941b960902a
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-25 15:24:28 +0530

    completing the 3d model section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
+import Model from './components/Model';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
