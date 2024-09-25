8b37398f4c96eeaf7bd32c5f864ae9414ffb66ee
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-25 17:22:29 +0530

    debugging an error

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
 import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
